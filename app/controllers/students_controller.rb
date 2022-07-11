21a020bf6d278ce251d396f50dffe5897bcf1130
Matthew <mattj20@gmail.com>
2022-07-10 17:33:44 -0700

    commit tree e131eb96b42fe65517ca455ee927b25f5186448b

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,13 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student = set_student
+  end
+
+  def activate
+    @student = set_student
+    @student.activate
+    redirect_to student_path(@student)
   end
 
   private
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,16 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def activate
+    if self.active == true
+      self.update(active: false)
+    else
+      self.update(active: true)
+    end
+  end
+
+  def active_status
+    self.active == true ? "This student is currently active." : "This student is currently inactive."
+  end
 end
